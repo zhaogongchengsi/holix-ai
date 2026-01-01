10c8687845082db145f5ee6e43b549a098499a3c
zhaogongchengsi <zzh1586169624@163.com>
2026-01-01 18:43:49 +0800

    fix: 统一代码风格，调整组件格式化和导入顺序

--- src/routes/chat/$id.tsx
+++ src/routes/chat/$id.tsx
@@ -1,18 +1,20 @@
+import { createFileRoute } from "@tanstack/react-router";
 import useChat from "@/store/chat";
-import { createFileRoute, useRouteContext, useRouterState } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/chat/$id")({
-  component: Component,
+	component: Component,
 });
 
 function Component() {
-  const { id } = Route.useParams();
-  const chat = useChat((state) => state.chats.find((chat) => chat.uid === id));
+	const { id } = Route.useParams();
+	const chat = useChat((state) => state.chats.find((chat) => chat.uid === id));
 
-  console.log("Chats in $id route:", chat);
+	console.log("Chats in $id route:", chat);
 
-  return <div>
-	<h1>Chat ID: {id}</h1>
-	{JSON.stringify(chat)}
-  </div>;
+	return (
+		<div>
+			<h1>Chat ID: {id}</h1>
+			{JSON.stringify(chat)}
+		</div>
+	);
 }
