40bebde50582c1e67bb59fd86d81fbc54a462f81
zhaogongchengsi <zzh1586169624@163.com>
2026-01-03 13:56:28 +0800

    style: 格式化代码缩进，统一代码风格

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import { useInitMessages, useMessageUpdates } from "@/hooks/message";
 import { router } from "./router";
 
 export default function App() {
-	// 初始化所有数据
-	useInitChats();
-	useInitMessages();
+  // 初始化所有数据
+  useInitChats();
+  useInitMessages();
 
-	// 监听所有更新事件
-	useChatUpdates();
-	useMessageUpdates();
+  // 监听所有更新事件
+  useChatUpdates();
+  useMessageUpdates();
 
-	return (
-		<ThemeProvider>
-			<RouterProvider router={router} defaultPreload="intent" />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} defaultPreload="intent" />
+    </ThemeProvider>
+  );
 }
